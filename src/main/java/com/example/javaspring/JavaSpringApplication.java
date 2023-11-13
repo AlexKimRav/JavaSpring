6b32c16669a6411b99d4bceb341c2e996ea1638a
AlexKimRav <ravenq1113@gmail.com>
2023-11-13 17:45:57 +0900

    TelegramBot Added

--- src/main/java/com/example/javaspring/JavaSpringApplication.java
+++ src/main/java/com/example/javaspring/JavaSpringApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class JavaSpringApplication {
-
     public static void main(String[] args) {
 
         SpringApplication.run(JavaSpringApplication.class, args);
--- src/main/java/com/example/javaspring/controllers/HelloController.java
+++ src/main/java/com/example/javaspring/controllers/HelloController.java
@@ -1,12 +1,38 @@
 package com.example.javaspring.controllers;
 
+import com.example.javaspring.Service.UserService;
+import com.example.javaspring.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/")
+    private final UserService userService;
+    @Autowired
+    public HelloController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/hello")
     public String index() {
         return "Hello from Spring Boot";
+    }
+
+    @GetMapping("/allUsers")
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/getUser")
+    public Optional<User> getUser(@RequestParam Long id) {
+        return userService.findById(id);
     }
 }
